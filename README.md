a0f76444ef2dc708950fee71cf1457e432b16bc8
Nesser Andrade <nesser24601@live.com>
2021-09-16 21:41:47 -0300

    código JS e alterações no CSS

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# projeto1-devinhouse
+# Projeto 1
+
 Primeiro projeto do Módulo 1 do DevInHouse
+
+# Como funciona?
+
+É uma lista de tarefas que permite adicionar tarefas, marcar as tarefas feitas e apagá-las.
---
+++ script.js
@@ -0,0 +1,65 @@
+let dados = document.querySelector("#inputDados")
+let resultado = document.querySelector("#lista")
+dados.focus()
+
+function carregarLista() {
+    let carregaLista = localStorage.getItem("listadetarefas")
+    if (carregaLista) {
+        resultado.innerHTML = carregaLista
+    }
+}
+
+function addTarefa () {
+    let dadosValor = dados.value.trim()
+    if (dadosValor.length === 0) {
+        alert("Digite a tarefa!")
+    }
+    else {
+        let li = document.createElement("li")
+        let div = document.createElement("div")
+        div.className = "divResultado"
+        let input = document.createElement("input")
+        input.className = "checkbox"
+        input.type = "checkbox"
+        input.addEventListener("change", checkEvento)
+        let p = document.createElement("p")
+        p.className = "entrada"
+        let botao = document.createElement("button")
+        botao.className = "botaoTarefa"
+        botao.setAttribute("onclick", "deletarTarefa(this)")
+        botao.innerHTML = "X"
+        li.appendChild(div)
+        div.appendChild(input)
+        div.appendChild(p)
+        div.appendChild(botao)
+        let item = document.createTextNode(dadosValor)
+        p.appendChild(item)
+        resultado.appendChild(li)
+        salvarLista()
+    }
+}
+
+function checkEvento(evento) {
+    const paragrafo = evento.target.parentElement.children[1]
+    if (evento.target.checked) {
+        paragrafo.classList.add("marcado")
+      } else {
+        paragrafo.classList.remove("marcado")
+      }
+    salvarLista()
+}
+
+
+function deletarTarefa(apagar) {
+    let apagarConfirma = confirm("Deseja apagar a tarefa?")
+    if (apagarConfirma) {
+        apagar.parentNode.parentNode.parentNode.removeChild(apagar.parentNode.parentNode)
+        salvarLista()
+    }
+}
+
+function salvarLista() {
+    localStorage.setItem('listadetarefas', resultado.innerHTML)
+}
+
+carregarLista()
